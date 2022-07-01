3fe2f963c792422c7c3e2d7b2ede3c24d2ebcb59
F7021995 Matheus Ataides Reis <matheus.ataides@bb.com.br>
2022-07-01 12:33:53 -0300

    Desafios do modulo 1.

---
+++ desafios_modulo1/src/b_MuitoSystemOutFacil.java
@@ -0,0 +1,67 @@
+import static java.lang.System.out;
+import static java.lang.Math.sqrt;
+
+public class b_MuitoSystemOutFacil {
+
+    public static void main(String[] args) {
+        String[] nomes = {"A", "B", "C", "D", "E"};
+        Double[] pesos = {70.3, 55.6, 65.0, 80.0, 90.0};
+        Double[] alturas = {1.70, 1.60, 1.75, 1.80, 1.85};
+
+        Pessoa[] pessoas = new Pessoa[5];
+
+        for (int i = 0; i < 5; i++) {
+            pessoas[i].setNome(nomes[i]);
+            pessoas[i].setPeso(pesos[i]);
+            pessoas[i].setAltura(alturas[i]);
+
+            Double imc = pessoas[i].getPeso() / sqrt(pessoas[i].getAltura());
+
+            if(imc < 18.5 || imc > 25) {
+                out.printf("%s seu IMC é %.1f %n", pessoas[i].getNome(), imc);
+                out.println("Você não possui IMC ideal (entre 18.5 e 25)");
+            }
+        }
+    }
+
+    void programaMuitoSystemOut() {
+        out.println("print fácil");
+        out.println("Importando static ...System.out; dá pra facilitar o print");
+        out.println("Muitos");
+        out.println("Prints");
+        out.println("Assim");
+        out.println("É bom importar o static System.out");
+    }
+
+}
+
+
+class Pessoa {
+
+
+    public void setNome(String nome) {
+    }
+
+    public void setIdade(int idade) {
+
+    }
+
+    public void setPeso(Double peso) {
+    }
+
+    public double getAltura() {
+        return 3.9;
+    }
+
+    public Double getPeso() {
+        return 3.0;
+    }
+
+    public Object getNome() {
+        return "meu nome";
+    }
+
+    public void setAltura(Double altura) {
+    }
+}
+
---
+++ desafios_modulo1/src/e_PrintVirgulaFacil.java
@@ -0,0 +1,21 @@
+import static java.lang.System.out;
+
+public class e_PrintVirgulaFacil {
+
+    public static void main(String[] args) {
+        String[] frutas = {"Abacaxi", "Maça", "Pera", "Tomate", "Mamão"};
+
+        for (int i = 0; i < 5; i++) {
+            out.println("Fila da puta man... tem como imprimir vírgula facim");
+            if(i > 0)
+                System.out.print(", ");
+            System.out.print(frutas[i]);
+        }
+
+    }
+
+    void me(int a, double b){}
+
+    void me(double b, int a){}
+
+}
